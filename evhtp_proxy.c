d4afd453efc591827633c9c40948d2cb1b46b9af
Jesse <boycht@gmail.com>
2015-11-18 11:17:42 +0800

    relay HTTP body in chunk, so download large file is possible

--- evhtp_proxy.c
+++ evhtp_proxy.c
@@ -9,7 +9,10 @@
 #include <event2/dns.h>
 
 
+#define MAX_OUTPUT (512*1024)
+
 void relay(struct bufferevent *b_in, struct evdns_base *evdns_base, const char *hostname, int port);
+
 #ifdef USE_THREAD
 static struct evdns_base  * evdnss[128] = {};
 #else
@@ -17,8 +20,83 @@ static struct evdns_base* evdns = NULL;
 #endif
 
 static void 
-print_error(evhtp_request_t * req, evhtp_error_flags errtype, void * arg) {
-	printf("evhtp error\n");
+print_backend_error(evhtp_request_t * req, evhtp_error_flags errtype, void * arg) {
+	evhtp_request_t * frontend_req = (evhtp_request_t *)arg;
+	printf("evhtp backend error\n");
+}
+
+static void 
+print_frontend_error(evhtp_request_t * req, evhtp_error_flags errtype, void * arg) {
+	evhtp_request_t * backend_req = (evhtp_request_t *)arg;
+	printf("evhtp frontend error\n");
+
+	evhtp_request_pause(backend_req);
+	evhtp_connection_t *ev_conn = evhtp_request_get_connection(backend_req);
+	evhtp_connection_free(ev_conn);
+}
+
+static evhtp_res resume_backend_request(evhtp_connection_t * conn, void * arg) {
+	evhtp_request_t * backend_req = (evhtp_request_t *)arg;
+
+	printf("resume backend request\n");
+	evhtp_request_resume(backend_req); // bug, client can't evhtp_request_resume
+
+	evhtp_unset_hook(&conn->hooks, evhtp_hook_on_write);
+	
+	return EVHTP_RES_OK;
+}
+
+
+static evhtp_res
+print_data(evhtp_request_t * req, evbuf_t * buf, void * arg) {
+	evhtp_request_t * frontend_req = (evhtp_request_t *)arg;
+	size_t len = evbuffer_get_length(buf);
+
+	printf("relay http body, got %zu bytes\n", len);
+	//fwrite(evbuffer_pullup(buf, len), 1, len, stdout);
+
+	evhtp_send_reply_chunk(frontend_req, buf);
+	
+	evbuffer_drain(buf, -1); // remove readed data
+
+	if(evbuffer_get_length(bufferevent_get_output(evhtp_request_get_bev(frontend_req))) > MAX_OUTPUT) {
+		printf("too many data, stop backend request\n");
+		evhtp_request_pause(req);
+
+		evhtp_set_hook(&evhtp_request_get_connection(frontend_req)->hooks, evhtp_hook_on_write, resume_backend_request, req);
+	}
+
+	return EVHTP_RES_OK;
+}
+
+static evhtp_res print_headers(evhtp_request_t * backend_req, evhtp_headers_t * hdr, void * arg) {
+	evhtp_request_t * frontend_req = (evhtp_request_t *)arg;
+	evhtp_header_t *header = NULL;
+
+    printf("all headers ok\n");
+    evhtp_kv_t * kv;
+
+    //TAILQ_FOREACH(kv, hdr, next) {
+    //    printf("%*s:%s\n", kv->klen, kv->key, kv->val);
+    //}
+
+    evhtp_headers_add_headers(frontend_req->headers_out, hdr);
+
+	//// Content-Length will be auto set by libevhtp
+    //if((header = evhtp_kvs_find_kv(frontend_req->headers_out, "Transfer-Encoding"))) {
+	//    evhtp_header_rm_and_free(frontend_req->headers_out, header);
+    //}
+    if((header = evhtp_kvs_find_kv(frontend_req->headers_out, "Connection"))) {
+	    evhtp_header_rm_and_free(frontend_req->headers_out, header);
+    }
+
+
+    printf("backend http response.\n");
+    //evhtp_send_reply(frontend_req, EVHTP_RES_OK);
+    evhtp_send_reply_chunk_start(frontend_req, evhtp_request_status(backend_req));
+    evhtp_request_resume(frontend_req);
+
+    return EVHTP_RES_OK;
 }
 
 int
@@ -35,13 +113,13 @@ make_request(evbase_t         * evbase,
              void             * arg) {
     evhtp_connection_t * conn;
     evhtp_request_t    * request;
-evhtp_header_t *header = NULL;
+	evhtp_header_t *header = NULL;
+    evhtp_request_t * frontend_req = (evhtp_request_t *)arg;
 
     conn         = evhtp_connection_new_dns(evbase, evdns, host, port);
     conn->thread = evthr;
     request      = evhtp_request_new(cb, arg);
 
-    evhtp_set_hook(&request->hooks, evhtp_hook_on_error, print_error, NULL);
 
     evhtp_headers_add_headers(request->headers_out, headers);
     if((header = evhtp_kvs_find_kv(request->headers_out, "Connection"))) {
@@ -59,6 +137,13 @@ evhtp_header_t *header = NULL;
 
     evbuffer_prepend_buffer(request->buffer_out, body);
 
+	// hook
+    evhtp_set_hook(&request->hooks, evhtp_hook_on_error, print_backend_error, arg);
+    evhtp_set_hook(&request->hooks, evhtp_hook_on_headers, print_headers, arg);
+    evhtp_set_hook(&request->hooks, evhtp_hook_on_read, print_data, arg);
+
+    evhtp_set_hook(&frontend_req->hooks, evhtp_hook_on_error, print_frontend_error, request);
+
     printf("Making backend request...\n");
     evhtp_make_request(conn, request, method, path);
     printf("async.\n");
@@ -68,34 +153,13 @@ evhtp_header_t *header = NULL;
 
 static void
 backend_cb(evhtp_request_t * backend_req, void * arg) {
-	evhtp_header_t *header = NULL;
+	//evhtp_header_t *header = NULL;
     evhtp_request_t * frontend_req = (evhtp_request_t *)arg;
 
-    evbuffer_prepend_buffer(frontend_req->buffer_out, backend_req->buffer_in);
-    evhtp_headers_add_headers(frontend_req->headers_out, backend_req->headers_in);
+    printf("finish http response.\n");
+    evhtp_send_reply_chunk_end(frontend_req);
 
-	// Content-Length will be auto set by libevhtp
-    if((header = evhtp_kvs_find_kv(frontend_req->headers_out, "Transfer-Encoding"))) {
-	    evhtp_header_rm_and_free(frontend_req->headers_out, header);
-    }
-    if((header = evhtp_kvs_find_kv(frontend_req->headers_out, "Connection"))) {
-	    evhtp_header_rm_and_free(frontend_req->headers_out, header);
-    }
-
-    /*
-     * char body[1024] = { '\0' };
-     * ev_ssize_t len = evbuffer_copyout(frontend_req->buffer_out, body, sizeof(body));
-     * printf("Backend %zu: %s\n", len, body);
-     */
-
-    printf("backend http response.\n");
-    //evhtp_send_reply(frontend_req, EVHTP_RES_OK);
-    evhtp_send_reply(frontend_req, evhtp_request_status(backend_req));
-    evhtp_request_resume(frontend_req);
-
-    //evhtp_connection_t * conn = backend_req->conn;
-    //evhtp_request_free(backend_req);
-    //evhtp_connection_free(conn);
+    evhtp_unset_hook(&frontend_req->hooks, evhtp_hook_on_error);
 }
 
 static void
